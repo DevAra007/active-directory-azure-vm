1282662528ee715dadd0c0ba0076116765a193bf
Oluwabusayo Derek Shofowora <160604855+DevAra007@users.noreply.github.com>
2025-02-17 21:22:32 -0500

    Updated Create_AD_Users.ps1
    Password never expires
    Password does not need to be changed on first login as Azure VM would not allow.
    Password can be changed later in OS

--- Create_AD_Users.ps1
+++ Create_AD_Users.ps1
@@ -1,25 +1,43 @@
 # Import user data
-$users = Import-Csv .\User_List.csv -Header FirstName,LastName
+$users = Import-Csv .\User_List.csv
 
-# Create IT Organizational Unit
-New-ADOrganizationalUnit -Name "IT"
+# Define domain and organizational unit (OU)
+$domain = "devdomain.local"
+$ouPath = "OU=IT,DC=devdomain,DC=local"
+
+# Create IT Organizational Unit (if it doesn’t exist)
+if (-not (Get-ADOrganizationalUnit -Filter "Name -eq 'IT'")) {
+    New-ADOrganizationalUnit -Name "IT" -Path "DC=devdomain,DC=local" -ProtectedFromAccidentalDeletion $false
+}
 
 # Loop through users and create accounts
 foreach ($user in $users) {
-    $username = ($user.FirstName.Substring(0,1) + $user.LastName).ToLower()
-    New-ADUser -SamAccountName $username `
-               -UserPrincipalName "$username@mydomain.local" `
-               -Name "$($user.FirstName) $($user.LastName)" `
-               -GivenName $user.FirstName `
-               -Surname $user.LastName `
-               -Path "OU=IT,DC=mydomain,DC=local" `
-               -AccountPassword (ConvertTo-SecureString "pass1925" -AsPlainText -Force) `
-               -Enabled $true
+    if ($user.FirstName -and $user.LastName) {
+        $username = ($user.FirstName.Substring(0,1) + $user.LastName).ToLower()
+        $password = ConvertTo-SecureString "pass1925" -AsPlainText -Force
+
+        New-ADUser -SamAccountName $username `
+                   -UserPrincipalName "$username@$domain" `
+                   -Name "$($user.FirstName) $($user.LastName)" `
+                   -GivenName $user.FirstName `
+                   -Surname $user.LastName `
+                   -Path $ouPath `
+                   -AccountPassword $password `
+                   -Enabled $true `
+                   -PasswordNeverExpires $true `
+                   -ChangePasswordAtLogon $false
+    }
 }
 
-# Create IT_Department group
-New-ADGroup -Name "IT_Department" -GroupScope Global
+# Create IT_Department group (if it doesn’t exist)
+if (-not (Get-ADGroup -Filter "Name -eq 'IT_Department'")) {
+    New-ADGroup -Name "IT_Department" -GroupScope Global -GroupCategory Security -Path $ouPath
+}
 
-# Add users to IT_Department group
-Get-ADUser -Filter * | Where-Object { $_.DistinguishedName -like "*OU=IT,*" } | 
-    ForEach-Object { Add-ADGroupMember -Identity "IT_Department" -Members $_ }
+# Add users in IT OU to IT_Department group
+$usersInOU = Get-ADUser -SearchBase $ouPath -Filter *
+foreach ($user in $usersInOU) {
+    Add-ADGroupMember -Identity "IT_Department" -Members $user
+}
+
+Write-Host "User creation and group assignment completed successfully!"
